a40cac40ded68c252cff122a50bda7728010e1b0
Szovo <136929732+Szovo@users.noreply.github.com>
2024-03-03 12:58:17 -0500

    Update Data Cleaning Portfolio Project Queries.sql

--- Data Cleaning Portfolio Project Queries.sql
+++ Data Cleaning Portfolio Project Queries.sql
@@ -1,14 +1,6 @@
-/*
-
-Cleaning Data in SQL Queries
-
-*/
-
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
 
---------------------------------------------------------------------------------------------------------------------------
 
 -- Standardize Date Format
 
@@ -28,8 +20,6 @@ Add SaleDateConverted Date;
 Update NashvilleHousing
 SET SaleDateConverted = CONVERT(Date,SaleDate)
 
-
- --------------------------------------------------------------------------------------------------------------------------
 
  -- Data Cleaning
 
@@ -53,11 +43,6 @@ JOIN PortfolioProject.dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 Where a.PropertyAddress is null
-
-
-
-
---------------------------------------------------------------------------------------------------------------------------
 
 -- Breaking out Address into Individual Columns (Address, City, State)
 
@@ -130,27 +115,15 @@ Add OwnerSplitState Nvarchar(255);
 Update NashvilleHousing
 SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 
-
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
 
-
-
-
---------------------------------------------------------------------------------------------------------------------------
-
-
 -- Change Y and N to Yes and No in "Sold as Vacant" field
-
 
 Select Distinct(SoldAsVacant), Count(SoldAsVacant)
 From PortfolioProject.dbo.NashvilleHousing
 Group by SoldAsVacant
 order by 2
-
-
-
 
 Select SoldAsVacant
 , CASE When SoldAsVacant = 'Y' THEN 'Yes'
@@ -165,13 +138,6 @@ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 	   When SoldAsVacant = 'N' THEN 'No'
 	   ELSE SoldAsVacant
 	   END
-
-
-
-
-
-
------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Remove Duplicates
 
@@ -195,101 +161,16 @@ From RowNumCTE
 Where row_num > 1
 Order by PropertyAddress
 
-
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
-
-
-
-
----------------------------------------------------------------------------------------------------------
 
 -- Delete Unused Columns
 
-
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
 
-
 ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
---- Importing Data using OPENROWSET and BULK INSERT	
-
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
-
---GO 
-
-
----- Using BULK INSERT
-
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (
---      FIELDTERMINATOR = ',',
---      ROWTERMINATOR = '\n'
---);
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
-
-
-
-
-
-
-
-
-
-
-
 
 
 
